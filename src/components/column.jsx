1fbb7b23674c7601d4ba65aa00b366a26ee48ca0
david-diep <ddkotan@gmail.com>
2020-08-30 12:19:27 -0700

    css changes

--- src/components/column.jsx
+++ src/components/column.jsx
@@ -64,7 +64,7 @@ class Column extends React.Component {
               <div
                 className={`
                   tasklist
-                  h-80
+                  h-90
                   p-1
                   ${snapshot.isDraggingOver ? 'bg-info':''}
                 `}
