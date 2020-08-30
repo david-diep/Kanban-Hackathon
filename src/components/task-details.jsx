bce98ed40ac9206f83dcf9aa9c3f83926285f344
Kevin Lenell <krlenell@gmail.com>
2020-08-29 21:52:09 -0700

    deleted extra stuff in task.jsx, will revive later in modal

---
+++ src/components/task-details.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default class TaskDetails extends React.Component{
+  constructor(props){
+    super(props)
+    this.handleClick= this.handleClick.bind(this)
+  }
+
+  handleClick(e){
+
+  }
+
+  render(){
+    return(
+      <h1>I fuckin hate making modals</h1>
+    )
+  }
+
+}
