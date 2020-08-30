5c4ab84ff9409482371f209d80b83e0b9495791c
david-diep <ddkotan@gmail.com>
2020-08-30 13:20:30 -0700

    Updates

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# Kanban-Hackathon
-Project for the Mintbean Olympics Hackathon 8-28-20
---
+++ precache-manifest.7b646270fa28e8a3ab9f2cf89e3d08f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a8f2a033dee9634f33a0dcdfe5df72",
+    "url": "/kanban-hackathon/index.html"
+  },
+  {
+    "revision": "17f8f82a3f19aecd8fd7",
+    "url": "/kanban-hackathon/static/css/main.da9b9c07.chunk.css"
+  },
+  {
+    "revision": "a520d4f424c792fc5b61",
+    "url": "/kanban-hackathon/static/js/2.b5386be6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kanban-hackathon/static/js/2.b5386be6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f8f82a3f19aecd8fd7",
+    "url": "/kanban-hackathon/static/js/main.3ff6e1c6.chunk.js"
+  },
+  {
+    "revision": "80f9d2d3be6c47d208cb",
+    "url": "/kanban-hackathon/static/js/runtime-main.8b26f085.js"
+  }
+]);
